0dc6107b2ec31850b333c6ffc3f548ed9e62918a
Robin Morse <000morse@gmail.com>
2026-06-21 09:11:48 +0900

    10fps animation? performance issues

--- render.js
+++ render.js
@@ -4,7 +4,19 @@ const ctx = canvas.getContext("2d");
 
 const image = new Image();
 
+// globals
+jitterRate = 10; // format is [xMin, xMax], [yMin, yMax]
+jitterMod = 2;
+
+jitterRate1 = jitterRate * jitterMod;
+jitterRate2 = jitterRate * jitterMod ** 2;
+
+jitterGlyph = [[jitterRate, jitterRate], [jitterRate, jitterRate]];
+jitterStar1 = [[jitterRate1, jitterRate1], [jitterRate1, jitterRate1]];
+jitterStar2 = [[jitterRate2, jitterRate2], [jitterRate2, jitterRate2]];
+
 // helper functions
+// drawing functions
 const drawPoint = (a) => ctx.fillRect(a[0] - 3, a[1] - 3, 6, 6); // draw a point at [x, y] coordinate pair a
 const drawLine = (a, b) => { // draw a line between [x, y] coordinate pairs a and b
 	ctx.moveTo(a[0], a[1]);
@@ -20,17 +32,55 @@ const drawShape = (points) => { // draw a polygon between each [x, y] coordinate
 		// drawPoint(points[i]);
 		drawLine(points[i - 1], points[i]);
 	}
+	
+	return points; // we'll use the return to store these for the next frame
 }
-onmousemove = function(e){console.log("mouse location:", e.clientX - 210, e.clientY - 16)} // shows (roughly) mouse pointer position in canvas; saves a lot of headache
+// animation functions
+const brownianVib = (points, lastPoints, jitter) => { // points: array of points, lastPoints: array of points at last frame, jitter: 2x2 jitter array; returns jittered array
+	let newPoints = []; // return array
+	
+	for (let i = 0; i < points.length; i++) {
+		jitterX = 2 * jitter[0][1] * Math.random() - jitter[0][0];
+		jitterY = 2 * jitter[1][1] * Math.random() - jitter[1][0];
+		
+		newPoints[i] = [0.5 * (points[i][0] + lastPoints[i][0] + jitterX),
+						0.5 * (points[i][1] + lastPoints[i][1] + jitterY)];
+	}
+	
+	return newPoints;
+}
+const update = () => { // updates the frame, draws everything
+	// clear the canvas
+	ctx.beginPath();
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-// placeholder/guide; drawing image as the background
-//image.src = "./61star.png";
-//image.onload = function() {ctx.drawImage(this, 0, 0)}; // draw the image when it loads
+	// draw each glyph
+	// draw letters/numbers
+	sixPtsD = drawShape(brownianVib(sixPts, sixPtsD, jitterGlyph)); // 6
+	onePtsD = drawShape(brownianVib(onePts, onePtsD, jitterGlyph)); // 1
+	bPtsD = drawShape(brownianVib(bPts, bPtsD, jitterGlyph)); // b
+	i1PtsD = drawShape(brownianVib(i1Pts, i1PtsD, jitterGlyph)); // i #1
+	rPtsD = drawShape(brownianVib(rPts, rPtsD, jitterGlyph)); // r
+	dPtsD = drawShape(brownianVib(dPts, dPtsD, jitterGlyph)); // d
+	i2PtsD = drawShape(brownianVib(i2Pts, i2PtsD, jitterGlyph)); // i #2
+	ePtsD = drawShape(brownianVib(ePts, ePtsD, jitterGlyph)); // e
+	// draw stars
+	star1Pts1D = drawShape(brownianVib(star1Pts1, star1Pts1D, jitterGlyph)); // big star 1
+	star1Pts2D = drawShape(brownianVib(star1Pts2, star1Pts2D, jitterStar1)); // big star 2
+	star1Pts3D = drawShape(brownianVib(star1Pts3, star1Pts3D, jitterStar2)); // big star 3
+	star2PtsD = drawShape(brownianVib(star2Pts, star2PtsD, jitterGlyph)); // i #1 star
+	star3PtsD = drawShape(brownianVib(star3Pts, star3PtsD, jitterGlyph)); // i #2 star
+	
+	setTimeout(() => {
+		requestAnimationFrame(update);
+	}, 100);
+}
+
+// onmousemove = function(e){console.log("mouse location:", e.clientX - 210, e.clientY - 16)} // shows (roughly) mouse pointer position in canvas; saves a lot of headache
 
 // define glyphs in points
 // letters/numbers
-// 6
-sixPts = [
+sixPts = [ // 6
 	[146, 211],
 	[70, 350],
 	[51, 420],
@@ -46,8 +96,7 @@ sixPts = [
 	[138, 357],
 	[214, 211]
 ]
-// 1
-onePts = [
+onePts = [ // 1
 	[340, 292],
 	[268, 357],
 	[324, 357],
@@ -55,8 +104,7 @@ onePts = [
 	[324, 618],
 	[408, 292]
 ]
-// b
-bPts = [
+bPts = [ // b
 	[547, 216],
 	[470, 543],
 	[531, 543],
@@ -72,15 +120,13 @@ bPts = [
 	[565, 399],
 	[613, 216]
 ]
-// i #1
-i1Pts = [
+i1Pts = [ // i #1
 	[729, 380],
 	[683, 543],
 	[745, 543],
 	[790, 380]
 ]
-// r
-rPts = [
+rPts = [ // r
 	[833, 380],
 	[789, 543],
 	[854, 543],
@@ -92,8 +138,7 @@ rPts = [
 	[884, 415],
 	[894, 380]
 ]
-// d
-dPts = [
+dPts = [ // d
 	[1148, 216],
 	[1100, 408],
 	[1070, 382],
@@ -108,15 +153,13 @@ dPts = [
 	[1129, 543],
 	[1212, 216]
 ]
-// i #2
-i2Pts = [
+i2Pts = [ // i #2
 	[1218, 380],
 	[1177, 543],
 	[1237, 543],
 	[1277, 380]
 ]
-// e
-ePts = [
+ePts = [ // e
 	[1383, 378],
 	[1343, 394],
 	[1311, 427],
@@ -132,8 +175,7 @@ ePts = [
 	[1421, 386]
 ]
 // stars
-// big star
-star1Pts1 = [
+star1Pts1 = [ // big star inner
 	[454, 171],
 	[444, 235],
 	[403, 259],
@@ -145,7 +187,7 @@ star1Pts1 = [
 	[511, 210],
 	[473, 226]
 ]
-star1Pts2 = [
+star1Pts2 = [ // big star middle
 	[453, 105],
 	[439, 229],
 	[335, 264],
@@ -157,7 +199,7 @@ star1Pts2 = [
 	[581, 152],
 	[476, 217]
 ]
-star1Pts3 = [
+star1Pts3 = [ // big star large
 	[451, 55],
 	[454, 242],
 	[377, 133],
@@ -179,8 +221,7 @@ star1Pts3 = [
 	[537, 99],
 	[462, 239]
 ]
-// i #1 star
-star2Pts = [
+star2Pts = [ // i #1 star
 	[777, 275],
 	[758, 338],
 	[729, 257],
@@ -202,8 +243,7 @@ star2Pts = [
 	[817, 313],
 	[767, 339]
 ]
-// i #2 star
-star3Pts = [
+star3Pts = [ // i #2 star
 	[1287, 251],
 	[1254, 336],
 	[1233, 276],
@@ -225,6 +265,9 @@ star3Pts = [
 	[1334, 288],
 	[1262, 335]
 ]
+// variables for last frame
+let sixPtsD = sixPts, onePtsD = onePts, bPtsD = bPts, i1PtsD = i1Pts, rPtsD = rPts, dPtsD = dPts, i2PtsD = i2Pts, ePtsD = ePts, 
+	star1Pts1D = star1Pts1, star1Pts2D = star1Pts2, star1Pts3D = star1Pts3, star2PtsD = star2Pts, star3PtsD = star3Pts; // we'll also overload these to bool for if we don't want to draw
 
 // drawing settings
 ctx.fillStyle = "white";
@@ -232,30 +275,4 @@ ctx.strokeStyle = "white";
 ctx.lineWidth = 2;
 ctx.imageSmoothingEnabled = false;
 
-// draw each glyph
-// draw letters/numbers
-// 6
-drawShape(sixPts);
-// 1
-drawShape(onePts);
-// b
-drawShape(bPts);
-// i #1
-drawShape(i1Pts);
-// r
-drawShape(rPts);
-// d
-drawShape(dPts);
-// i #2
-drawShape(i2Pts);
-// e
-drawShape(ePts);
-// draw stars
-// big star
-drawShape(star1Pts1);
-drawShape(star1Pts2);
-drawShape(star1Pts3);
-// i #1 star
-drawShape(star2Pts);
-// i #2 star
-drawShape(star3Pts);
+update();
